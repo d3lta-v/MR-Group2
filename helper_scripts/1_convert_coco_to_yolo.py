e9819c56610b0a2f12286d15afa8c10940209e5d
Pan Ziyue <random.rrr3@gmail.com>
2025-12-07 12:31:57 +0800

    chore: updated helper script names

--- helper_scripts/1_convert_coco_to_yolo.py
+++ helper_scripts/1_convert_coco_to_yolo.py
@@ -1,7 +1,7 @@
 from ultralytics.data.converter import convert_coco
 
 convert_coco(
-    labels_dir="dataset/coco/",
-    save_dir="dataset/yolo/",
+    labels_dir="../dataset/coco/",
+    save_dir="../dataset/yolo/",
     use_segments=True, use_keypoints=False, cls91to80=False
 )
---
+++ helper_scripts/3_zip_datasets.py
@@ -0,0 +1,3 @@
+# This script zips the dataset/yolo directory
+# after which it also zips the autosplit train/val/test text files.
+# and finally includes data.yaml in the zip.
